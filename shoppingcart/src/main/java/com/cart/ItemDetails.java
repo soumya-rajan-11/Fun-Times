a5dd63aab27f052a5e6dded1a1646d250a39b117
soumya-rajan-11 <54357304+soumya-rajan-11@users.noreply.github.com>
2019-08-21 16:33:39 +0530

    Create ItemDetails.java

---
+++ shoppingcart/src/main/java/com/cart/ItemDetails.java
@@ -0,0 +1,22 @@
+    
+package com.cart;
+
+import java.math.BigDecimal;
+
+public enum ItemDetails {
+    SOUP(BigDecimal.valueOf(0.65)),
+    BREAD(BigDecimal.valueOf(0.80)),
+    MILK(BigDecimal.valueOf(1.30)),
+    APPLE(BigDecimal.valueOf(0.10));
+
+    private final BigDecimal price;
+
+    ItemDetails(BigDecimal price) {
+	this.price = price;
+    }
+
+    public BigDecimal getPrice() {
+	return price;
+    }
+
+}
